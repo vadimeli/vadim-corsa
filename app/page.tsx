14939e4f3dbc5fb22dc522247901a065099b1a66
tom-voicer <tom@voicer.com>
2025-07-23 11:19:02 +0300

    feat: init

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-4xl font-bold">Frontend chat assignment</h1>
+    </div>
+  );
+}
